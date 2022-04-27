107499094df8c036ade8fe08adcbbe7ef7277a1c
Klaus Kunze <klauspkunze@gmail.com>
2022-04-26 20:26:18 -0600

    Added humidity to the weather page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast= (latitude, longitude, callback) => {
     } else if(body.error) {
         callback('Unable to find location', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees F. It feels like ' + body.current.feelslike + ' degrees F.')    
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees F. It feels like ' + body.current.feelslike + ' degrees F. The humidity is ' + body.current.humidity + ' percent')    
             
             // console.log('Weather: ' + body.current.weather_descriptions[0])
             // console.log('It is currently ' + body.current.temperature + ' degrees F')
